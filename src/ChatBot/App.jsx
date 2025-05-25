9513e2540a528d8baec883400d5129d6732dd585
cooldkt143 <deepakkumartripathy1008@gmail.com>
2025-05-25 17:36:04 +0530

    Adding vertual chatbot icon

--- src/ChatBot/App.jsx
+++ src/ChatBot/App.jsx
@@ -42,24 +42,20 @@ export const App = () => {
 
   return (
     <div className={`fixed bottom-5 right-5 z-50 ${showChatbot ? '' : ''}`}>
-    <span
-      onClick={() => setShowChatbot(prev => !prev)}
-      className={`fixed z-50 flex items-center justify-center text-xl font-bold shadow-lg transition-all duration-300 ease-in-out
-        ${showChatbot
-          ? 'bottom-7 right-7 w-[60px] h-[60px] rounded-full bg-rose-700 text-white hover:bottom-8 hover:right-8 hover:w-[65px] hover:h-[65px] hover:rounded-full'
-          : 'bottom-7 right-7 w-[60px] h-[60px] rounded-full bg-rose-500 text-white hover:bottom-8 hover:right-8 hover:w-[65px] hover:h-[65px] hover:rounded-full'}
-      `}
-    >
-      <span className="material-symbols-rounded cursor-pointer text-white text-2xl">
-        {showChatbot ? "close" : "chat_bubble"}
+      <span
+        onClick={() => setShowChatbot(prev => !prev)}
+        className={`fixed z-50 flex items-center justify-center text-xl font-bold shadow-lg transition-all duration-300 ease-in-out
+          ${showChatbot
+            ? 'bottom-7 hidden lg:flex right-7 w-[60px]  h-[60px] rounded-full bg-rose-700 text-white hover:bottom-8 hover:right-8 hover:w-[65px] hover:h-[65px] hover:rounded-full'
+            : 'bottom-7 right-7 w-[60px] h-[60px] rounded-full bg-rose-500 text-white hover:bottom-8 hover:right-8 hover:w-[65px] hover:h-[65px] hover:rounded-full'}
+        `}
+      >
+        <span className="material-symbols-rounded cursor-pointer text-white text-2xl">
+          {showChatbot ? "close" : "chat_bubble"}
+        </span>
       </span>
-    </span>
-
-
-
-
       {showChatbot && (
-        <div className="fixed bottom-24 right-5 sm:max-w-full sm:max-h-full lg:h-[600px] lg:w-[420px] bg-white rounded-2xl shadow-2xl overflow-hidden flex flex-col">
+        <div className="fixed bottom-0 lg:bottom-24 right-0 lg:right-5 w-full h-full lg:h-[600px] lg:w-[420px] bg-white rounded-2xl shadow-2xl overflow-hidden flex flex-col">
           <div className="flex items-center justify-between bg-rose-700 px-6 py-4 text-white">
             <div className="flex items-center gap-3">
               <div className="w-9 h-9 p-1.5 bg-white rounded-full fill-rose-700">
@@ -68,12 +64,12 @@ export const App = () => {
               <h2 className="text-lg font-semibold">ChatBot</h2>
             </div>
             <span 
-            onClick={() => setShowChatbot(false)} className="material-symbols-rounded cursor-pointer bg-rose-700 hover:bg-rose-600 sm:rounded-full sm:w-[30px] sm:h-[30px] text-white text-2xl justify-center items-center flex">
+            onClick={() => setShowChatbot(false)} className="material-symbols-rounded cursor-pointer bg-rose-700 hover:bg-rose-600 rounded-full sm:rounded-full w-[30px] h-[30px] sm:w-[30px] sm:h-[30px] text-white text-2xl justify-center items-center flex">
               keyboard_arrow_down
             </span>
           </div>
 
-          <div ref={chatBodyRef} className="flex flex-col gap-3 p-6 h-[460px] overflow-y-auto">
+          <div ref={chatBodyRef} className="flex flex-col gap-3 p-6 lg:h-[460px] h-screen overflow-y-auto">
             <div className="flex gap-3 items-end">
               <div className="w-9 h-9 p-1.5 bg-rose-700 fill-white rounded-full">
                 <ChatbotIcon />
